3601b5adf80adfdb54bda72fe065e9b540fe2e73
Seraj Haqiqi <haqiqi.seraj@gmail.com>
2024-10-16 07:12:25 +0330

    add saram

--- internal/consumer/consumer.go
+++ internal/consumer/consumer.go
@@ -1,53 +1,68 @@
-package consumer
+package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"strings"
+	"time"
 
-	"gitea.com/logicamp/lc"
-	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
-	"go.uber.org/zap"
+	"github.com/IBM/sarama"
+	"github.com/reugn/go-streams/flow"
 )
 
-type message struct {
-	Msg string
-}
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
+	defer cancel()
 
-func (msg *message) String() string {
-	return msg.Msg
-}
+	hosts := []string{"127.0.0.1:9093"}
+	config := sarama.NewConfig()
+	config.Consumer.Group.Rebalance.Strategy = sarama.NewBalanceStrategyRoundRobin()
+	config.Consumer.Offsets.Initial = sarama.OffsetNewest
+	config.Producer.Return.Successes = true
+	config.Version, _ = sarama.ParseKafkaVersion("3.6.2")
+	groupID := "testConsumer"
 
-type Feeder struct {
-	producer    *lc.KafkaProducer
-	consumer    *lc.KafkaConsumer
-	FeedChannel chan any
-}
-
-func NewFeeder(kafkaHost, groupId string) *Feeder {
-	feeder := &Feeder{
-		producer:    lc.NewKafkaProducer(kafkaHost),
-		consumer:    lc.NewKafkaConsumer(kafkaHost, groupId),
-		FeedChannel: make(chan any),
+	consumerGroup, err := sarama.NewConsumerGroup(hosts, groupID, config)
+	if err != nil {
+		log.Fatal(err)
 	}
-	feeder.init()
 
-	return feeder
+	source := ext.NewSaramaSource(ctx, consumerGroup, []string{"test"}, nil)
+
+	// syncProducer, err := sarama.NewSyncProducer(hosts, config)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// sink := ext.NewSaramaSink(syncProducer, "test2", nil)
+
+	toUpperMapFlow := flow.NewMap(toUpper, 1)
+	throttler := flow.NewThrottler(1, time.Second, 50, flow.Discard)
+	tumblingWindow := flow.NewTumblingWindow[*sarama.ConsumerMessage](time.Second * 5)
+	appendAsteriskFlatMapFlow := flow.NewFlatMap(appendAsterisk, 1)
+
+	channel := source.
+		Via(toUpperMapFlow).
+		Via(throttler).
+		Via(tumblingWindow).
+		Via(appendAsteriskFlatMapFlow)
+
+	for i := range channel {
+		fmt.Println(i)
+	}
 }
 
-func (f *Feeder) init() {
-	go func() {
-
-		err := f.consumer.Consume(f.onMessage, []string{"test-stream"}, "earliest", false)
-		if err != nil {
-			lc.Logger.Error("consume error", zap.Error(err))
-		}
-	}()
+var toUpper = func(msg *sarama.ConsumerMessage) *sarama.ConsumerMessage {
+	msg.Value = []byte(strings.ToUpper(string(msg.Value)))
+	return msg
 }
 
-func (f *Feeder) onMessage(message *kafka.Message) error {
-
-	value := string(message.Value)
-	fmt.Println("value is:", value)
-	f.FeedChannel <- value
-
-	return nil
+var appendAsterisk = func(inMessages []*sarama.ConsumerMessage) []*sarama.ConsumerMessage {
+	outMessages := make([]*sarama.ConsumerMessage, len(inMessages))
+	for i, msg := range inMessages {
+		msg.Value = []byte(string(msg.Value) + "*")
+		outMessages[i] = msg
+	}
+	return outMessages
 }
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"stock-processor/internal/consumer"
 	"strings"
 
 	ext "github.com/reugn/go-streams/extension"
